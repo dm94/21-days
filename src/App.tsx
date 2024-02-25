781e8e8d080834ba57f9ade92c4dc3d07a4e99b6
Jao <65263323+JaoAD@users.noreply.github.com>
2024-02-25 20:19:42 +0800

    Feat/i18n (#2)
    * translations
    
    * feat: i18n translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+
+function MyComponent() {
+  const { t, i18n } = useTranslation();
+
+  return <h1>{t("title")}</h1>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -22,14 +29,14 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <MyComponent />
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
