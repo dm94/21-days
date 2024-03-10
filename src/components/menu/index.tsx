b9b25b90519ece088ca0cd30ed1e1cfb0f2d4ab8
JaoAD <aka.jaomike@gmail.com>
2024-03-10 20:07:59 +0800

    Navigation bar

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,5 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import './style.css';
+import { useTranslation } from "react-i18next";
+import "./style.css";
 
 export default function Menu() {
   const { t } = useTranslation();
@@ -8,13 +8,13 @@ export default function Menu() {
     <nav>
       <ul className="menu">
         <li className="home-button button">
-          <a href="#calendar">{t('home')}</a>
+          <a href="#calendar">{t("menu.home")}</a>
         </li>
         <li className="configuration-button button">
-          <a href="#configuration">{t('configuration')}</a>
+          <a href="#configuration">{t("menu.config")}</a>
         </li>
         <li className="theory-button button">
-          <a href="#theory">{t('theory')}</a>
+          <a href="#theory">{t("menu.theory")}</a>
         </li>
       </ul>
     </nav>
